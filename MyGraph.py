eb7979f429dfc4e91112d6d8045ef34aade2622a
Ally <allison_benson1@baylor.edu>
2024-01-24 13:28:46 -0600

    updated git ignore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,12 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World")
+
+
+# 1) create a VE - py (-3 -m venv [command] myvenv [name of the environment]) (pc)
+# python3 -m venv (command) myvenv (name of the environment)
+# 2) activate your (VE myvenv/scripts/activate) (pc)
+# 3) install 3rd party library/module (source my venv/bin/activate)
+# have powershell on in security settings
+# pip3 install matplotlib to install
